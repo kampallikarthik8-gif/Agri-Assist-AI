27c633a10188dd5e8a8547a1512fe8d3725cf30d
Kampalli Karthik <kampallikarthik8@gmail.com>
2025-08-30 14:01:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -146,12 +146,12 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
               <DropdownMenuContent side="right" align="start" className="w-56">
                 <DropdownMenuLabel>My Account</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <Link href="/profile">
-                    <DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                    <Link href="/profile">
                         <Icons.User className="mr-2 h-4 w-4" />
                         <span>Profile</span>
-                    </DropdownMenuItem>
-                </Link>
+                    </Link>
+                </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
         </SidebarFooter>

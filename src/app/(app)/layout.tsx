8e062c3bc7bed4ea9a618de594748266f3167cf9
Kampalli Karthik <kampallikarthik8@gmail.com>
2025-08-31 06:54:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -74,6 +74,15 @@ function AppName() {
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <SidebarProvider>
